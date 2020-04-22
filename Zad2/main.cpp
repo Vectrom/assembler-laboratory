5bf891bd56cbfa7749c759ae749cba11c2508834
Vectrom <daplociennik@gmail.com>
2020-04-22 18:47:07 +0200

    Add zad2

---
+++ Zad2/main.cpp
@@ -0,0 +1,13 @@
+
+void reverse_asm(int array[], int size) {
+    //tutaj dodaj wstawke asemblerowa
+}
+
+void reverse_cpp(int array[], int size) {
+    //tutaj dodaj kod funkcji w C++
+}
+
+int main() {
+    //tutaj przygotuj duze tablice, wykonaj wczesniej utworzone metody
+    //zmierz czas wykonywania poszczegolnych funkcji i pokaz go na standardowym wyjsciu
+}
